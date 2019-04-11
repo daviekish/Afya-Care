f1a135284cfd53bd9ef6b3caa1286250f46e9114
leon <leonmonyoro@gmail.com>
2019-04-11 15:47:42 +0300

    Added sign-up button and its contents to about page

--- a-js/scripts-a.js
+++ a-js/scripts-a.js
@@ -1,1 +1,9 @@
 
+    function myFunction() {
+        var x = document.getElementById("tab-signup-cont");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
